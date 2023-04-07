594fe489ba80858980ed2e9cd5e019bc548d0d0b
AliiMuhammed <mohamedtaha8000@gmail.com>
2023-04-08 00:39:26 +0200

    Add Router.js & notFound page

--- src/shared/CourseCard.jsx
+++ src/shared/CourseCard.jsx
@@ -1,15 +1,16 @@
 import "../style/courseCard.css";
 import { Link } from 'react-router-dom'
 import {SlNotebook} from "react-icons/sl"
+import { courses } from "../core/data/data";
 
-const CourseCard = ({title,description,instractor,durations,Image,className}) => {
+const CourseCard = ({title,id,description,instractor,durations,Image,className}) => {
   return (
     <div className={`course-card ${className}`}>
       <div className="course-card-image">
         <img src={Image} alt="course-image" />
       </div>
       <div className="course-card-content">
-        <h3><Link to={"/courses"}>{title}</Link></h3>
+        <h3><Link to={"/courses/"+id}>{title}</Link></h3>
         <div className="course-meta">
             <span>By <span className='course-instractor'>{instractor}</span></span>
             <span className='course-lessons'><SlNotebook/>{durations}</span>
