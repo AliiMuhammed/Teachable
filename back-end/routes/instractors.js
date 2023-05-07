4c9abb42155cf33ccc81027f107ffd231d6b563c
Abdelrahman435 <ah0383723@gmail.com>
2023-05-07 22:02:52 +0300

    updates

--- back-end/routes/instractors.js
+++ back-end/routes/instractors.js
@@ -7,45 +7,6 @@ const upload = require("../middleware/uploadImages");
 const util = require("util");
 const fs = require("fs");
 
-// CREATE INSTRACTOR
-// router.post("",
-//  admin,
-//  upload.single("image"),
-//   body("name")
-//   .isString()
-//   .withMessage("Please enter a valid Instractor name"),
-// //   body("description")
-// //   .isString()
-// //   .withMessage("Please enter a valid description")
-// //   .isLength({ min: 20 })
-// //   .withMessage("Description must be at least 20 characters"),
-//    async (req, res) => {
-//     try{
-//     const errors = validationResult(req);
-//         if (!errors.isEmpty()) {
-//             return res.status(400).json({errors: errors.array()});
-//         }
-//     if(!req.file){
-//         return res.status(400).json({errors: ["Please upload an image"]});
-//     }
-//     const instractor = {
-//         name: req.body.name,
-//         email: req.body.email,
-//         password: req.body.password,
-//         phone: req.body.phone,
-//         type: req.body.type,
-//         image_url: req.file.filename,
-//     };
-//     const query = util.promisify(conn.query).bind(conn);// transfer query mysql to --> promise to use (await,async)
-//     await query ("insert into users set ?",instractor)
-//     res.status(200).json({
-//         msg:"Instractor created",
-//     });
-// } catch(err){
-//          res.status(500).json(err);
-// }
-// });
-
 // UPDATE instarctor    
 router.put(
 "/:id",// params
@@ -66,7 +27,7 @@ router.put(
     const instractor = await query ("select * from users where id =?", [
         req.params.id
     ]);
-    if(!instractor[0]){
+    if(!instractor[0] || instractor[0].type !== "instractor"){
       res.status(400).json({msg: "Instractor not found"});
     }
 
@@ -101,7 +62,7 @@ router.delete("/:id",// params
     const query = util.promisify(conn.query).bind(conn);// transfer query mysql to --> promise to use (await,async)
     const instractor = await query ("select * from users where id =?",[req.params.id])
 
-    if(!instractor[0]){
+    if(!instractor[0] || instractor[0].type !== "instractor"){
         return  res.status(400).json({errors: ["Instractor not found"]});
     }
 
@@ -144,7 +105,7 @@ router.get("/:id", async (req, res, ) => {
      const query = util.promisify(conn.query).bind(conn);// transfer query mysql to --> promise to use (await,async)
     const instractor = await query ("select * from users where id =?",[req.params.id])
     
-    if(!instractor[0]){
+    if(!instractor[0] || instractor[0].type !== "instractor"){
         return res.status(400).json({errors: ["Instractor not found"]});
     }
     instractor[0].image_url = "http://" + req.hostname + ":4002/" + instractor[0].image_url;
@@ -159,21 +120,15 @@ catch(err){
 router.get('/view/:id', async(req, res) => {
     try{
         let students = [];
-        let stud_name = [];
     const query = util.promisify(conn.query).bind(conn);// transfer query mysql to --> promise to use (await,async)
     const instractor = await query ("select course_id from instractors_courses where instractor_id =?",[req.params.id])
 
-    if(!instractor[0]){
+    if(!instractor[0] || instractor[0].type !== "instractor"){
         return res.status(400).json({errors: ["Instractor not found"]});
     }
 
     for(let i = 0; i < instractor.length; i++){
         students[i] = await query("SELECT name, student_id from courses, users_courses where id =? and course_id  = ?", [instractor[i].course_id, instractor[i].course_id])
-        // stud_name[i] = await query("select name from users where id = ?",[students[i].student_id])
-        // students[i] = students[i] + stud_name[i];
-    //     students.map(course => {
-    //       course[0].image_url = "http://" + req.hostname + ":4002/" + course[0].image_url;
-    //   })
     }
 
     res.status(200).json(students)
