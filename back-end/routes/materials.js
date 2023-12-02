50651a84cf301dd6b433e07af78c8036d3cd166c
Abdelrahman435 <ah0383723@gmail.com>
2023-12-02 20:36:26 +0200

    Materials

--- back-end/routes/materials.js
+++ back-end/routes/materials.js
@@ -6,4 +6,8 @@ const materials = require("../controller/materialsController");
 router.post("/add/:course_id", upload.single("file"), materials.addMaterial);
 
 router.get("/:course_id", materials.getMaterials);
+
+router.patch("/update/:id", upload.single("file"), materials.updateMaterial);
+
+router.delete("/:id", materials.deleteMaterials);
 module.exports = router;
